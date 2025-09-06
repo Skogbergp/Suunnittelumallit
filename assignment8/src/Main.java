770cafd7b7d5e3eacb748b5aea4bf906e5b9e906
Patrik Skogberg <skogbergp@gmail.com>
2025-09-06 23:47:17 +0300

    Prompt user for player count input during game initialization

--- assignment8/src/Main.java
+++ assignment8/src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
          FightGame game = new FightGame();
-            game.play(2);
+        Scanner input = new Scanner(System.in);
+        int playerCount = 0;
+
+        while (true) {
+            System.out.print("How many players would you like to play? ");
+            if (input.hasNextInt()) {
+                playerCount = input.nextInt();
+                if (playerCount > 0) break;
+                else System.out.println("Please enter a number greater than 0.");
+            } else {
+                System.out.println("That's not a number!");
+                input.next(); // discard invalid input
+            }
+        }
+
     }
     
 }
