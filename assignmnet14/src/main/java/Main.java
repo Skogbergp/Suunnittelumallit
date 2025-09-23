992fce4d687ef382b30f7afea398a89c69aaeaa4
Patrik Skogberg <skogbergp@gmail.com>
2025-09-23 13:53:42 +0300

    Refactor main method to build and display Gaming and Office computers

--- assignmnet14/src/main/java/Main.java
+++ assignmnet14/src/main/java/Main.java
@@ -2,14 +2,19 @@ public class Main {
     public static void main(String[] args) {
         ComputerBuilder gamingBuilder = new GamingComputerBuilder();
         ComputerBuilder officeBuilder = new OfficeComputerBuilder();
+
         Computer GamingPC;
         Computer OfficePC;
+
         ComputerDirector director = new ComputerDirector(gamingBuilder);
         director.buildComputer();
+
         director = new ComputerDirector(officeBuilder);
         director.buildComputer();
+
         OfficePC = officeBuilder.getComputer();
         GamingPC = gamingBuilder.getComputer();
+
         System.out.println(GamingPC);
         System.out.println(OfficePC);
 
